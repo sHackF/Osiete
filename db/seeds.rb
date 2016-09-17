3c4b44321c7bfe7a7b70ca9e10a2307bf9f5b6a7
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-09-17 16:50:41 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create name: :sample_user, email: :none
+User.create name: '宝石 太郎', email: :none
+Tag.create name: :ruby
+Tag.create name: :料理
+Tag.create name: :python
+Give.create user_id: 2, title: :ruby, about: 'rails教えます'
+Give.create user_id: 2, title: :life, about: '人生相談しませんか'
+Want.create user_id: 1, title: :python, about: '機械学習が教えてください'
+Want.create user_id: 1, title: :zsh, about: 'イケイケなzshrcありませんか'
